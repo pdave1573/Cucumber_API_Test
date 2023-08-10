4f66c30e9c829918cc80aafc98690da943d54bfb
Pranav Dave <pdave1573@gmail.com>
2023-08-10 15:29:05 -0600

    Change to email
    Change to email

--- src/test/java/util/utility.java
+++ src/test/java/util/utility.java
@@ -9,7 +9,7 @@ import io.restassured.response.Response;
 public class utility {
 	
 	public void valueCheck(Response response, String key, String value) {
-		JsonPath js = new JsonPath(response.asString());
+		JsonPath js = new JsonPath(response.asString()) ;
 		Assert.assertEquals(js.get(key), value);
 	}
 }
